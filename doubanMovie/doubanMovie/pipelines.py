6180a8787bd6b113b337d0c9e0d93ee29d5392d6
Rory <1992706806@qq.com>
2018-07-21 01:47:05 +0800

    save to mongodb

--- doubanMovie/doubanMovie/pipelines.py
+++ doubanMovie/doubanMovie/pipelines.py
@@ -4,8 +4,15 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import pymongo
+
+client = pymongo.MongoClient('localhost', 27017)
+doubanMovieDB = client['doubanMovieDB']
+top250 = doubanMovieDB['top250']
 
 
 class DoubanmoviePipeline(object):
     def process_item(self, item, spider):
+        data = dict(item)
+        top250.insert(data)
         return item
