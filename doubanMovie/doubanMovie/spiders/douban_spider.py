405d128983c548bec7fba736a71c8264fd27c477
Rory <1992706806@qq.com>
2018-07-26 11:37:53 +0800

    change to xpath

--- doubanMovie/doubanMovie/spiders/douban_spider.py
+++ doubanMovie/doubanMovie/spiders/douban_spider.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 import scrapy
-from bs4 import BeautifulSoup
 from doubanMovie.items import DoubanmovieItem
 
 
@@ -10,30 +9,26 @@ class DoubanSpiderSpider(scrapy.Spider):
     start_urls = ['http://movie.douban.com/top250']  # 入口url
 
     def parse(self, response):  # 进行解析
-        content = BeautifulSoup(response.text, 'html.parser')
-        # print(content.text)
-        item_list = content.select('div.item')  # 得到一个页面的25个item
+        print(response.status, response.headers)
+        item_list = response.xpath("//div[@class='item']")  # 得到一个页面的25个item
         print(len(item_list))
         for item in item_list:
             movie_item = DoubanmovieItem()
-            movie_item['id'] = item.select('div.pic em')[0].text
-            movie_item['image'] = item.select('div.pic img')[0]['src']
-            movie_item['href'] = item.select('div.pic a')[0]['href']
-            movie_item['name'] = item.select('div.hd a span')[0].text
+            movie_item['id'] = item.xpath("./div/em/text()").extract_first()
+            movie_item['image'] = item.xpath("./div/a/img/@src").extract_first()
+            movie_item['href'] = item.xpath("./div/a/@href").extract_first()
+            movie_item['name'] = item.xpath(".//div[@class='hd']/a/span[1]/text()").extract_first()
 
-            introduce = item.select('div.bd p')[0].text.split()
-            movie_item['introduce'] = ''.join(introduce)
+            introduce = item.xpath(".//div[starts-with(@class, 'bd')]/p[1]/text()").extract()
+            movie_item['introduce'] = [''.join(i.strip('...').split()) for i in introduce]
 
-            movie_item['star'] = item.select('div.star span.rating_num')[0].text
-            movie_item['commentNum'] = item.select('div.star span:nth-of-type(4)')[0].text.rstrip('人评价')
-            if item.find('p.quote span'):
-                movie_item['quote'] = item.select('p.quote span')[0].text
-            else:
-                movie_item['quote'] = 'none'
+            movie_item['star'] = float(item.xpath(".//span[@class='rating_num']/text()").extract_first())
+            movie_item['commentNum'] = int(item.xpath(".//div[@class='star']/span[last()]/text()").extract_first().rstrip("人评价"))
+            movie_item['quote'] = item.xpath(".//p[@class='quote']/span[@class='inq']/text()").extract_first()
 
-            yield movie_item    # 到pipelines，存储、去重等等操作
+            yield movie_item  # 到pipelines，存储、去重等等操作
 
-        next_href = content.select('span.next a')
+        next_href = response.xpath(".//span[@class='next']/a/@href").extract()
         if next_href:
-            next_href = content.select('span.next a')[0]['href']
+            next_href = next_href[0]
             yield scrapy.Request('http://movie.douban.com/top250' + next_href, callback=self.parse)  # 回调函数为parse
